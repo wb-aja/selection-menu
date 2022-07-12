bca6833f82faada4855ece13eacb11d3b65421ab
FlutLab.io <>
2022-07-12 19:40:16 +0000

    Autosave

--- lib/makanan.dart
+++ lib/makanan.dart
@@ -39,11 +39,16 @@ class Makanan extends StatelessWidget {
               onTap: () {},
               splashColor: Colors.blue,
               child: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: const <Widget>[
-                    Icon(Icons.emoji_food_beverage_outlined, size: 70, color: Colors.blueAccent),
-                    Text("Minuman", style: TextStyle(fontSize: 17.0)),
+                child: Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    Ink.image(
+                      image: AssetImage('assets/images/makanan/lauk_gulai_cumi.jpg'),
+                      height: 70,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(height: 8),
+                    Text("Gulai Cumi", style: TextStyle(fontSize: 17.0)),
                   ],
                 ),
               ),
@@ -55,11 +60,79 @@ class Makanan extends StatelessWidget {
               onTap: () {},
               splashColor: Colors.blue,
               child: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: const <Widget>[
-                    Icon(Icons.fastfood_outlined, size: 70, color: Colors.blueAccent),
-                    Text("Snack", style: TextStyle(fontSize: 17.0)),
+                child: Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    Ink.image(
+                      image: AssetImage('assets/images/makanan/lauk_ikan_tenggiri_asam.jpg'),
+                      height: 70,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(height: 8),
+                    Text("Ikan Tenggiri", style: TextStyle(fontSize: 17.0)),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.all(8),
+            child: InkWell(
+              onTap: () {},
+              splashColor: Colors.blue,
+              child: Center(
+                child: Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    Ink.image(
+                      image: AssetImage('assets/images/makanan/lauk_kembung_bakar.jpg'),
+                      height: 70,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(height: 8),
+                    Text("Kembung Bakar", style: TextStyle(fontSize: 17.0)),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.all(8),
+            child: InkWell(
+              onTap: () {},
+              splashColor: Colors.blue,
+              child: Center(
+                child: Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    Ink.image(
+                      image: AssetImage('assets/images/makanan/lauk_rendang.jpg'),
+                      height: 70,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(height: 8),
+                    Text("Rendang", style: TextStyle(fontSize: 17.0)),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.all(8),
+            child: InkWell(
+              onTap: () {},
+              splashColor: Colors.blue,
+              child: Center(
+                child: Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    Ink.image(
+                      image: AssetImage('assets/images/makanan/nasi_putih.jpg'),
+                      height: 70,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(height: 8),
+                    Text("Nasi Putih", style: TextStyle(fontSize: 17.0)),
                   ],
                 ),
               ),
