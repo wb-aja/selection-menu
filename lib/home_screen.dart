12d7e23667986310c83221d63b3c58e758573c17
FlutLab.io <>
2022-07-13 15:47:28 +0000

    Autosave

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -13,92 +13,107 @@ class HomeScreen extends StatelessWidget {
         title: new Text('Rumah Makan Lamak Bana'),
       ),
       body: Center(
-        child: GridView.count(
-          padding: const EdgeInsets.all(25),
-          crossAxisCount: 2,
-          children: <Widget>[
-            Card(
-              margin: const EdgeInsets.all(8),
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (_) => Makanan()),
-                  );
-                },
-                splashColor: Colors.blue,
-                child: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: const <Widget>[
-                      Icon(Icons.food_bank_outlined, size: 70, color: Colors.blueAccent),
-                      Text("Makanan", style: TextStyle(fontSize: 17.0)),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Card(
-              margin: const EdgeInsets.all(8),
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (_) => Minuman()),
-                  );
-                },
-                splashColor: Colors.blue,
-                child: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: const <Widget>[
-                      Icon(Icons.emoji_food_beverage_outlined, size: 70, color: Colors.blueAccent),
-                      Text("Minuman", style: TextStyle(fontSize: 17.0)),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Card(
-              margin: const EdgeInsets.all(8),
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (_) => Snack()),
-                  );
-                },
-                splashColor: Colors.blue,
-                child: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: const <Widget>[
-                      Icon(Icons.fastfood_outlined, size: 70, color: Colors.blueAccent),
-                      Text("Snack", style: TextStyle(fontSize: 17.0)),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                SizedBox(
-                  width: 150,
-                  height: 30,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.red,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                    ),
-                    onPressed: () {
-                      var box = Hive.box('userBox').clear();
-
+        child: Column(
+          children: [
+            GridView.count(
+              padding: const EdgeInsets.all(25),
+              crossAxisCount: 2,
+              children: <Widget>[
+                Card(
+                  margin: const EdgeInsets.all(8),
+                  child: InkWell(
+                    onTap: () {
                       Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(builder: (_) => LoginScreen()),
+                        MaterialPageRoute(builder: (_) => Makanan()),
                       );
                     },
-                    child: Text('Logout'),
+                    splashColor: Colors.blue,
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: const <Widget>[
+                          Icon(Icons.food_bank_outlined, size: 70, color: Colors.blueAccent),
+                          Text("Makanan", style: TextStyle(fontSize: 17.0)),
+                        ],
+                      ),
+                    ),
                   ),
+                ),
+                Card(
+                  margin: const EdgeInsets.all(8),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (_) => Minuman()),
+                      );
+                    },
+                    splashColor: Colors.blue,
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: const <Widget>[
+                          Icon(Icons.emoji_food_beverage_outlined, size: 70, color: Colors.blueAccent),
+                          Text("Minuman", style: TextStyle(fontSize: 17.0)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: const EdgeInsets.all(8),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (_) => Snack()),
+                      );
+                    },
+                    splashColor: Colors.blue,
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: const <Widget>[
+                          Icon(Icons.fastfood_outlined, size: 70, color: Colors.blueAccent),
+                          Text("Snack", style: TextStyle(fontSize: 17.0)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    SizedBox(
+                      width: 150,
+                      height: 30,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.red,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        onPressed: () {
+                          var box = Hive.box('userBox').clear();
+
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(builder: (_) => LoginScreen()),
+                          );
+                        },
+                        child: Text('Logout'),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Nama : Trimurti Sari',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28, color: Colors.black),
                 ),
               ],
             ),
