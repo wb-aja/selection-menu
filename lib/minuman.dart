093d483e6be530de79ddc79614a29f51ed65932b
FlutLab.io <>
2022-07-14 07:05:43 +0000

    Autosave

--- lib/minuman.dart
+++ lib/minuman.dart
@@ -114,7 +114,7 @@ class Minuman extends StatelessWidget {
                       Navigator.pop(context);
                     },
                     child: const Text(
-                      'Home',
+                      'Menu',
                       style: TextStyle(color: Colors.white, fontSize: 15),
                     ),
                   ),
