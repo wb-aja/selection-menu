fcb37de4e00a39eb26d126bbb90708bc9f6e3044
FlutLab.io <>
2022-07-14 12:38:32 +0000

    Autosave

--- lib/makanan.dart
+++ lib/makanan.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'home_screen.dart';
 
 class Makanan extends StatelessWidget {
   @override
@@ -8,129 +9,164 @@ class Makanan extends StatelessWidget {
       appBar: new AppBar(
         title: new Text('Rumah Makan Lamak Bana'),
       ),
-      body: GridView.count(
-        padding: const EdgeInsets.all(25),
-        crossAxisCount: 2,
-        children: <Widget>[
-          Card(
-            margin: const EdgeInsets.all(8),
-            child: InkWell(
-              onTap: () {},
-              splashColor: Colors.blue,
-              child: Center(
-                child: Column(
-                  children: [
-                    Text("Ayam Bakar", style: TextStyle(fontSize: 17.0)),
-                    SizedBox(height: 8),
-                    Ink.image(
-                      image: AssetImage('assets/images/makanan/lauk_ayam_bakar.jpg'),
-                      height: 70,
-                      fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          GridView.count(
+            padding: const EdgeInsets.all(25),
+            crossAxisCount: 2,
+            children: <Widget>[
+              Card(
+                margin: const EdgeInsets.all(8),
+                child: InkWell(
+                  onTap: () {},
+                  splashColor: Colors.blue,
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Text("Ayam Bakar", style: TextStyle(fontSize: 17.0)),
+                        SizedBox(height: 8),
+                        Ink.image(
+                          image: AssetImage('assets/images/makanan/lauk_ayam_bakar.jpg'),
+                          height: 70,
+                          fit: BoxFit.cover,
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ),
-            ),
+              Card(
+                margin: const EdgeInsets.all(8),
+                child: InkWell(
+                  onTap: () {},
+                  splashColor: Colors.blue,
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Text("Gulai Cumi", style: TextStyle(fontSize: 17.0)),
+                        SizedBox(height: 8),
+                        Ink.image(
+                          image: AssetImage('assets/images/makanan/lauk_gulai_cumi.jpg'),
+                          height: 70,
+                          fit: BoxFit.cover,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: const EdgeInsets.all(8),
+                child: InkWell(
+                  onTap: () {},
+                  splashColor: Colors.blue,
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Text("Ikan Tenggiri", style: TextStyle(fontSize: 17.0)),
+                        SizedBox(height: 8),
+                        Ink.image(
+                          image: AssetImage('assets/images/makanan/lauk_ikan_tenggiri_asam.jpg'),
+                          height: 70,
+                          fit: BoxFit.cover,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: const EdgeInsets.all(8),
+                child: InkWell(
+                  onTap: () {},
+                  splashColor: Colors.blue,
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Text("Kembung Bakar", style: TextStyle(fontSize: 17.0)),
+                        SizedBox(height: 8),
+                        Ink.image(
+                          image: AssetImage('assets/images/makanan/lauk_kembung_bakar.jpg'),
+                          height: 70,
+                          fit: BoxFit.cover,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: const EdgeInsets.all(8),
+                child: InkWell(
+                  onTap: () {},
+                  splashColor: Colors.blue,
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Text("Rendang", style: TextStyle(fontSize: 17.0)),
+                        SizedBox(height: 8),
+                        Ink.image(
+                          image: AssetImage('assets/images/makanan/lauk_rendang.jpg'),
+                          height: 70,
+                          fit: BoxFit.cover,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: const EdgeInsets.all(8),
+                child: InkWell(
+                  onTap: () {},
+                  splashColor: Colors.blue,
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Text("Nasi Putih", style: TextStyle(fontSize: 17.0)),
+                        SizedBox(height: 8),
+                        Ink.image(
+                          image: AssetImage('assets/images/makanan/nasi_putih.jpg'),
+                          height: 70,
+                          fit: BoxFit.cover,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-          Card(
-            margin: const EdgeInsets.all(8),
-            child: InkWell(
-              onTap: () {},
-              splashColor: Colors.blue,
-              child: Center(
-                child: Column(
-                  children: [
-                    Text("Gulai Cumi", style: TextStyle(fontSize: 17.0)),
-                    SizedBox(height: 8),
-                    Ink.image(
-                      image: AssetImage('assets/images/makanan/lauk_gulai_cumi.jpg'),
-                      height: 70,
-                      fit: BoxFit.cover,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: Container(
+                  height: 50,
+                  //width: MediaQuery.of(context).size.width,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        )),
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (_) => HomeScreen()),
+                      );
+                    },
+                    child: const Text(
+                      'Home',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
                     ),
-                  ],
+                  ),
                 ),
               ),
-            ),
-          ),
-          Card(
-            margin: const EdgeInsets.all(8),
-            child: InkWell(
-              onTap: () {},
-              splashColor: Colors.blue,
-              child: Center(
-                child: Column(
-                  children: [
-                    Text("Ikan Tenggiri", style: TextStyle(fontSize: 17.0)),
-                    SizedBox(height: 8),
-                    Ink.image(
-                      image: AssetImage('assets/images/makanan/lauk_ikan_tenggiri_asam.jpg'),
-                      height: 70,
-                      fit: BoxFit.cover,
-                    ),
-                  ],
-                ),
+              SizedBox(
+                height: 20,
               ),
-            ),
-          ),
-          Card(
-            margin: const EdgeInsets.all(8),
-            child: InkWell(
-              onTap: () {},
-              splashColor: Colors.blue,
-              child: Center(
-                child: Column(
-                  children: [
-                    Text("Kembung Bakar", style: TextStyle(fontSize: 17.0)),
-                    SizedBox(height: 8),
-                    Ink.image(
-                      image: AssetImage('assets/images/makanan/lauk_kembung_bakar.jpg'),
-                      height: 70,
-                      fit: BoxFit.cover,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Card(
-            margin: const EdgeInsets.all(8),
-            child: InkWell(
-              onTap: () {},
-              splashColor: Colors.blue,
-              child: Center(
-                child: Column(
-                  children: [
-                    Text("Rendang", style: TextStyle(fontSize: 17.0)),
-                    SizedBox(height: 8),
-                    Ink.image(
-                      image: AssetImage('assets/images/makanan/lauk_rendang.jpg'),
-                      height: 70,
-                      fit: BoxFit.cover,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Card(
-            margin: const EdgeInsets.all(8),
-            child: InkWell(
-              onTap: () {},
-              splashColor: Colors.blue,
-              child: Center(
-                child: Column(
-                  children: [
-                    Text("Nasi Putih", style: TextStyle(fontSize: 17.0)),
-                    SizedBox(height: 8),
-                    Ink.image(
-                      image: AssetImage('assets/images/makanan/nasi_putih.jpg'),
-                      height: 70,
-                      fit: BoxFit.cover,
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            ],
           ),
         ],
       ),
